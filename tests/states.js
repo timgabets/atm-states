bff3d8e1548f32e5fe5141151ac17655f3cb9659
Tim Gabets <tim@gabets.ru>
2017-11-27 13:05:58 +0300

    parseState() is now returning Map()

--- tests/states.js
+++ tests/states.js
@@ -3,13 +3,9 @@ import StatesService from '../lib/states.js';
 
 const s = new StatesService();
 
-test('should pass the dummy test', t => {
-  t.is(true, true);
-});
-
-test('should pass the dummy test', t => {
-  t.is(s.parseState(), false);
-});
+/**
+ * getEntry() test cases
+ */
 
 test('should get entry 1', t => {
   t.is(s.getEntry('000ABCDEFGHIJKLMNOPQRSTUVWXY', 1), 'A');
@@ -50,4 +46,30 @@ test('should get entry 9', t => {
 test('should get entry 10', t => {
   t.is(s.getEntry('000ABCDEFGHIJKLMNOPQRSTUVWXY', 10), null);
 });
+
+/**
+ * parseState()
+ */
+test('should return null if state number is invalid', t => {
+  t.is(s.parseState('XYZA870500128002002002001127'), null);
+});
+
+test('should parse state A properly', t => {
+ let parsed = new Map();
+  
+  parsed.set('description', 'Card read state');
+  parsed.set('number', '000'); 
+  parsed.set('type', 'A');
+  parsed.set('screen_number', '870');
+  parsed.set('good_read_next_state', '500'); 
+  parsed.set('error_screen_number', '128');
+  parsed.set('read_condition_1', '002');
+  parsed.set('read_condition_2', '002');
+  parsed.set('read_condition_3', '002');
+  parsed.set('card_return_flag', '001');
+  parsed.set('no_fit_match_next_state', '127');
+  
+  t.deepEqual(s.parseState('000A870500128002002002001127'), parsed);
+});
+
  
